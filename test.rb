0f3eaa9494e51e84a4c39559856e2b3bad3ffe4b
redazures <jwu_nyc@yahoo.com>
2020-11-06 01:21:33 -0500

    first

---
+++ test.rb
@@ -0,0 +1,21 @@
+# cart=[3,17,30]
+# cart2=[4,18,31]
+
+array=[1000000001, 1000000002, 1000000003, 1000000004, 1000000005]
+
+puts array.sum
+# puts cart[0]-cart2[0]
+
+# def answer(a,b)
+#     index=0
+#     outcome=[0,0]
+#     while index<3
+#         score = a[index]-b[index]
+#         outcome[0]+=1 if score>0
+#         outcome[1]+=1 if score<0
+#         index+=1
+#     end
+#     outcome
+# end
+
+# puts answer(cart,cart2)
