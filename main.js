ba42fd0c6eb599157adc6743cff769e370fe7245
redazures <jwu_nyc@yahoo.com>
2020-12-09 23:24:23 -0500

    udemy search

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
     console.log('works for me')
 })
 
+function binarySearch(arr,tar){
+    // add whatever parameters you deem necessary - good luck!
+    let start =0
+    let end = arr.length-1
+    let middle=Math.floor((start+end/2))
+    while (arr[middle]!==tar && start<=end){
+        
+    }
+  }
