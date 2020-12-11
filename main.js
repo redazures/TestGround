50bcc354ca6b3942b553acdd884aa90bb94af381
redazures <jwu_nyc@yahoo.com>
2020-12-10 22:43:31 -0500

    bubblesort start

--- main.js
+++ main.js
@@ -2,12 +2,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
     console.log('works for me')
 })
 
-function binarySearch(arr,tar){
-    // add whatever parameters you deem necessary - good luck!
-    let start =0
-    let end = arr.length-1
-    let middle=Math.floor((start+end/2))
-    while (arr[middle]!==tar && start<=end){
-        
-    }
-  }
+function bubbleSort(arr){
+    
+return arr
+}
+
+const test= [3,6,1,9,50,44]
+
+console.log(bubbleSort(test))
