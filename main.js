3567a82dc55a2406b7c191eaa7dcdfc8887a223e
redazures <jwu_nyc@yahoo.com>
2020-12-11 19:32:29 -0500

    bubblesort update

--- main.js
+++ main.js
@@ -3,8 +3,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
 })
 
 function bubbleSort(arr){
-    
-return arr
+    for(var i = arr.length;i>0;i--){
+        for(var j = 0;j<i-1;j++){
+            if(arr[j]>arr[j+1]){
+                var temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1]=temp
+            }
+        }
+    }
+    return arr
 }
 
 const test= [3,6,1,9,50,44]
