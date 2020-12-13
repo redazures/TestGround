20801615145802210aa9b10076b7365c6ba48650
redazures <jwu_nyc@yahoo.com>
2020-12-12 21:32:41 -0500

    bubblesort 2

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
 })
 
 function bubbleSort(arr){
+    const swap = (arr,idx1,idx2)=>{
+        [arr[idx1],arr[idx2]]=[arr[idx2],arr[idx1]]
+    }
     for(var i = arr.length;i>0;i--){
         for(var j = 0;j<i-1;j++){
             if(arr[j]>arr[j+1]){
-                var temp = arr[j]
-                arr[j] = arr[j+1]
-                arr[j+1]=temp
+                swap(arr,j,j+1)
             }
         }
     }
