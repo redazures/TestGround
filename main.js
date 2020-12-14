82b1aa0dbf2d3abe39575829756b6f8cc1aafba8
redazures <jwu_nyc@yahoo.com>
2020-12-13 23:19:08 -0500

    bubblesort revised

--- main.js
+++ main.js
@@ -16,6 +16,21 @@ function bubbleSort(arr){
     return arr
 }
 
-const test= [3,6,1,9,50,44]
+function bubbleSort2(arr){
+    const swap = (arr,switch1,switch2)=>{
+        [arr[switch1],arr[switch2]] = [arr[switch2],arr[switch1]]
+    }
 
-console.log(bubbleSort(test))
+    for (var i=arr.length;i>0;i--){
+        for(var j = 0;j<i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr,j,j+1)
+            }
+        }
+    }
+    return arr
+}
+
+const test= [3,6,1,9,50,44,25]
+
+console.log(bubbleSort2(test))
