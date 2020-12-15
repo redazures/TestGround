c8ee111d40d45476b6e16fa30cc1b7d474ced64a
redazures <jwu_nyc@yahoo.com>
2020-12-15 00:23:31 -0500

    more sort

--- main.js
+++ main.js
@@ -2,20 +2,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     console.log('works for me')
 })
 
-function bubbleSort(arr){
-    const swap = (arr,idx1,idx2)=>{
-        [arr[idx1],arr[idx2]]=[arr[idx2],arr[idx1]]
-    }
-    for(var i = arr.length;i>0;i--){
-        for(var j = 0;j<i-1;j++){
-            if(arr[j]>arr[j+1]){
-                swap(arr,j,j+1)
-            }
-        }
-    }
-    return arr
-}
-
 function bubbleSort2(arr){
     const swap = (arr,switch1,switch2)=>{
         [arr[switch1],arr[switch2]] = [arr[switch2],arr[switch1]]
@@ -31,6 +17,18 @@ function bubbleSort2(arr){
     return arr
 }
 
+function bubbleSort(arr){
+    const swap = (arr,one,two)=>{
+        [arr[one],arr[two]] = [arr[two],arr[one]]
+    }
+    for (var i=arr.length;i>0;i--){
+        for(var j =0;j<i-1;j++){
+            if(arr[j]>arr[j+1]) swap(arr,j,j+1)
+        }
+    }
+    return arr
+}
+
 const test= [3,6,1,9,50,44,25]
 
-console.log(bubbleSort2(test))
+console.log(bubbleSort(test))
