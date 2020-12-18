9e20d3958a8ccc278726fd922846fd57a8ba3b52
redazures <jwu_nyc@yahoo.com>
2020-12-17 19:20:15 -0500

    graphed done

--- main.js
+++ main.js
@@ -14,11 +14,28 @@ class Graph {
         this.adjacencyList[v1].push(v2)
         this.adjacencyList[v2].push(v1)
     }
+    removeEdge(v1,v2){
+        this.adjacencyList[v1] = this.adjacencyList[v1].filter(el=>el !== v2)
+        this.adjacencyList[v2] = this.adjacencyList[v2].filter(el=>el !== v1)
+    }
+    deleteVertex(vertex){
+        while(this.adjacencyList[vertex].length){
+            const removed = this.adjacencyList[vertex].pop()
+            this.removeEdge(vertex,removed)
+        }
+        delete this.adjacencyList[vertex]
+    }
 }
 
 let gunit=new Graph()
 gunit.addVertex("Jimbo")
 gunit.addVertex("Iroh")
 gunit.addVertex("Vinnie")
+gunit.addVertex("Kevin")
+gunit.addVertex("Victor")
 gunit.addEdge("Jimbo","Iroh")
+gunit.addEdge("Jimbo","Vinnie")
+gunit.addEdge("Iroh","Vinnie")
+gunit.addEdge("Kevin","Victor")
+gunit.deleteVertex("Jimbo")
 console.log(gunit)
