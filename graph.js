843c8cd57e28f6374325c9e49e24858a2eac715b
redazures <jwu_nyc@yahoo.com>
2020-12-17 21:10:12 -0500

    graph traverse

---
+++ graph.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('works for me')
+})
+
+class Graph {
+    constructor(){
+        this.adjacencyList={}
+    }
+    addVertex(vertex){
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex]=[]
+    }
+    addEdge(v1,v2){
+        // console.log("addedge",v1,v2,this.adjacencyList[v1])
+        this.adjacencyList[v1].push(v2)
+        this.adjacencyList[v2].push(v1)
+    }
+    removeEdge(v1,v2){
+        this.adjacencyList[v1] = this.adjacencyList[v1].filter(el=>el !== v2)
+        this.adjacencyList[v2] = this.adjacencyList[v2].filter(el=>el !== v1)
+    }
+    deleteVertex(vertex){
+        while(this.adjacencyList[vertex].length){
+            const removed = this.adjacencyList[vertex].pop()
+            this.removeEdge(vertex,removed)
+        }
+        delete this.adjacencyList[vertex]
+    }
+}
+
+let gunit=new Graph()
+gunit.addVertex("a")
+gunit.addVertex("b")
+gunit.addVertex("c")
+gunit.addVertex("d")
+gunit.addVertex("e")
+gunit.addVertex("f")
+gunit.addEdge("a","b")
+gunit.addEdge("a","c")
+gunit.addEdge("b","d")
+gunit.addEdge("c","e")
+gunit.addEdge("d","e")
+gunit.addEdge("d","f")
+gunit.addEdge("e","f")
+
+console.log(gunit)
