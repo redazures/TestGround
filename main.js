6178769cab1043fff54d5334092ded8a6a0c15cd
redazures <jwu_nyc@yahoo.com>
2020-12-21 03:13:15 -0500

    added some text

--- main.js
+++ main.js
@@ -4,11 +4,15 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 function minSubArrayLen(arr,n){
     var total = arr.reduce((a, b) => a + b, 0)
+    var totalRight=total
     if (total < n) return 0
-    for (let i=0;i<arra.length;i++){
-        
+    var right = arra.length-1
+    for (let i=0;i<right;i++){
+        totalRight-=arra[right]
+        total-=arra[left]
     }
     return arr.length
 }
+//redo problem
 
 console.log(minSubArrayLen([1,4,16,22,5,7,8,9,10],95))
