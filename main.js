13be12bbb9bf640fc214a14c24cb9f41cd3759cf
redazures <jwu_nyc@yahoo.com>
2020-12-23 23:32:54 -0500

    start convert

--- main.js
+++ main.js
@@ -2,39 +2,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
     console.log('works for me')
 })
 
-var isValid = function(s) {
-    let key ={"(":0,"[":0,"{":0}
-    let queue = []
-    for(let i=0;i<s.length;i++){
-        console.log(key[s[i]],s[i   ],queue[0])
-
-        if(s[i]==="("||s[i]==="["||s[i]==="{") {
-            key[s[i]]++
-            queue.push(s[i])
-        }
-        if(s[i]===")" && queue[0]==="(") {
-            key["("]--
-            queue.shift()
-            console.log("hit")
-        }
-        if(s[i]==="}" && queue[0]==="{") {
-            key["{"]--
-            queue.shift()
-            console.log("hit")
-        }
-        // console.log(s[i]==="]",queue[0]==="[")
-        if(s[i]==="]" && queue[0]==="[") {
-            key["["]--
-            queue.shift()
-            console.log("hit")
-        }
-        console.log(key,queue)
+var convert = function(s, numRows) {
+    for(let i =0;i<numRows;i++){
+        let count = numRows+1
+        let times = s.length
     }
-    for (var k in key){
-        if (key[k]>0) return false
-        console.log(queue.length)
-    }
-    return true
 };
 
-console.log(isValid("([])"))
+Input: s = "PAYPALISHIRING", numRows = 3
+Output = "PAHNAPLSIIGYIR"
+convert("PAYPALISHIRING",)
