3a53b2ce45dd9fde79d1e425c55936c4dd2cc3d6
redazures <jwu_nyc@yahoo.com>
2020-12-28 22:59:33 -0500

    added ruby test file

---
+++ rubytest.rb
@@ -0,0 +1,31 @@
+# What is the largest prime factor of the number 600851475143 ?
+
+value = 600851475143
+largest = Math.sqrt(value).to_i
+
+puts largest
+
+def is_prime(num)
+    (2..(num - 1)).each do |n|
+      return false if num % n == 0
+    end
+    true
+end
+
+i = 2
+
+prime =0
+while i < largest  do
+   if value%2==0
+        i=i+1
+        next
+   else
+        remainder = value%(i)
+        if remainder==0 && is_prime(i)
+            prime=i
+        end
+        i=i+1
+   end
+end
+
+puts prime
