a7cbf81bb7a368d3c3a20f248ee1bdd9d72f64da
James Wu <jwu_nyc@yahoo.com>
2021-01-19 22:54:44 -0500

    can create tree

--- main.js
+++ main.js
@@ -2,48 +2,51 @@ document.addEventListener('DOMContentLoaded', (event) => {
     console.log('works for me')
 })
 
-// var convert = function(s, numRows) {
-//     for(let i =0;i<numRows;i++){
-//         let count = numRows+1
-//         let times = s.length
-//     }
-// };
-
-// Input: s = "PAYPALISHIRING", numRows = 3
-// Output = "PAHNAPLSIIGYIR"
-// convert("PAYPALISHIRING",)
-
-function solve(s0, s1) {
-    let key = {}
-    let num = 0
-    let count = 0
-    for(let i=0;i<s0.length;i++){
-        key[s0[i]] ? key[s0[i]]++ : key[s0[i]] =1
-        count++
+class Node{
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
     }
-
-    for(let j=0;j<s1.length-s0.length+1;j++){
-        var test = Object.assign({},key)
-        var test2 = count
-        // console.log(test2)
-        for(let k=0;k<s0.length;k++){
-            if (test[s1[j+k]]===undefined){
-                break
-            } 
-            if (test[s1[j+k]]){
-                test2--
-                test[s1[j+k]]--
-            } 
-            if (test[s1[j+k]]<0){
-                break
-            } 
-            console.log(test2,num)
-            if(k===s0.length-1 && test2===0) num++
-        }
-    }
-    return num
 }
 
-console.log(solve("abc","bcabxabc"))
+class BinarySearchTree{
+    constructor(){
+        this.root=null
+    }
 
-//not sure if this folder is even needed if I am using leetcode to keep tract of my answers
+    insert(value){
+        var newNode = new Node(value)
+        if(this.root===null){
+            this.root=newNode
+            return this
+        }
+        var current = this.root
+        while(true){
+            if(value===current.value)return undefined
+            if(value<current.value){
+                if(current.left===null){
+                    current.left=newNode
+                    return this
+                }
+                current=current.left
+            }else{
+                if(current.right===null){
+                    current.right=newNode
+                    return this
+                }
+                current=current.right
+            }
+        }
+    }
+}
+
+var tree = new BinarySearchTree()
+tree.insert(10)
+tree.insert(5)
+tree.insert(13)
+tree.insert(7)
+tree.insert(11)
+tree.insert(2)
+tree.insert(16)
+console.log(tree)
