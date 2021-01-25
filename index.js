a12eef31c50a777329ff236da3b8e4317ad7b257
James Wu <jwu_nyc@yahoo.com>
2021-01-25 17:42:03 -0500

    binary search tree built

--- index.js
+++ index.js
@@ -14,6 +14,30 @@ class BinarySearchTree{
     constructor(){
         this.root=null
     }
+
+    add(value){
+        var newNode = new Node(value)
+        if(this.root===null) {
+            this.root = newNode
+        }
+        var current = this.root
+        while(true){
+            if(current.value===value)return undefined
+            if(value<current.value){
+                if(current.left===null){
+                    current.left=newNode
+                    return this
+                }
+                current=current.left
+            }else{
+                if(current.right===null){
+                    current.right=newNode
+                    return this
+                }
+                current=current.right
+            }
+        }
+    }
 }
 
 var tree = new BinarySearchTree()
