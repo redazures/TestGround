74d96d16c40c29019657d43b9dd742ec1f7ad451
James <jwu_nyc@yahoo.com>
2021-02-11 00:15:53 -0500

    check

--- main.js
+++ main.js
@@ -2,21 +2,47 @@ document.addEventListener('DOMContentLoaded', (event) => {
     console.log('works for me')
 })
 
-function mystery(input) {
-    // The input will be an array of strings
-    let index = 0
-    let flag = false
-    while ( index < input.length ) {
-        var word = input[index]
-        input.forEach(thisWord => {
-            flag = flag || thisWord === word
-            console.log(flag)
-        })
-        index++
-        // console.log("flag", flag)
+function solution(S) {
+    checkValid = (S)=>{
+        let key={}
+        for(let i=0;i<S.length;i++){
+            const letter = S[i]
+            // console.log(letter)
+            if(letter===letter.toLowerCase()){
+                if(!key[letter] && key[letter.toUpperCase()]!==0)key[letter.toUpperCase()]=1
+            }
+            else if(letter===letter.toUpperCase()){
+                key[letter.toUpperCase()]=0
+                // console.log(key)
+            }
+        }
+        for(let letter in key){
+            if(key[letter]>0) return false
+        }
+        
+        return true
     }
-    return flag
+
+    for(let i =0;i<S.length;i++){
+        console.log(S.substring(i,S.length))
+        // checkValid(S.substring(i,s.length))
+    }
+    // return checkValid(S)
+    //the ahove will check if the string is valid, 
+    //we need to make smaller strings and see if they are valid, if string is valid, then return string. 
 
 }
 
-// console.log(mystery(["string1", "string1", "worde"]))
+// function solution(S, K) {
+//     // write your code in JavaScript (Node.js 8.9.4)
+//     const days = ["Mon","Tues","Wed","Thu","Fri","Sat","Sun","Mon","Tues","Wed","Thu","Fri","Sat","Sun"]
+//     const stub = K%7
+//     for(let i=0;i<days.length-1;i++){
+//         if(days[i]===S)return days[stub+i]
+//     }
+// }
+
+console.log(solution("azABaabza"))
+// solution("TacoCat")
+// solution("AcZCbaBz")
+// solution("abcdefghijklmnopqrstuvwxyz")
