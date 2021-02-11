e302a75ab2c7e0e33ee708070fd60421915de6ea
James <jwu_nyc@yahoo.com>
2021-02-11 11:56:19 -0500

    updated balance

--- main.js
+++ main.js
@@ -3,33 +3,46 @@ document.addEventListener('DOMContentLoaded', (event) => {
 })
 
 function solution(S) {
-    checkValid = (S)=>{
+    if(S.length<2)return -1
+
+    checkBalance = (string)=>{
         let key={}
-        for(let i=0;i<S.length;i++){
-            const letter = S[i]
-            // console.log(letter)
+        for(let i=0;i<string.length;i++){
+            const letter = string[i]
             if(letter===letter.toLowerCase()){
                 if(!key[letter] && key[letter.toUpperCase()]!==0)key[letter.toUpperCase()]=1
+                key[letter.toLowerCase()]=0
             }
             else if(letter===letter.toUpperCase()){
+                if(!key[letter] && key[letter.toLowerCase()]!==0)key[letter.toLowerCase()]=1
                 key[letter.toUpperCase()]=0
-                // console.log(key)
             }
         }
+        
         for(let letter in key){
             if(key[letter]>0) return false
         }
-        
         return true
     }
 
-    for(let i =0;i<S.length;i++){
-        console.log(S.substring(i,S.length))
-        // checkValid(S.substring(i,s.length))
+    let shortestLength=S.length+1, string =""
+    
+    for(let i =0;i<S.length-1;i++){
+        for(let j=S.length;j>i;j--){
+            const testString=S.substring(i,j)
+            if (checkBalance(testString)){
+                if(testString.length<shortestLength){
+                    shortestLength = testString.length 
+                    string=testString
+                }
+            }
+        }
     }
+
     // return checkValid(S)
     //the ahove will check if the string is valid, 
     //we need to make smaller strings and see if they are valid, if string is valid, then return string. 
+    return string ? string.length : -1
 
 }
 
@@ -42,7 +55,8 @@ function solution(S) {
 //     }
 // }
 
+console.log(solution("aA"))
 console.log(solution("azABaabza"))
-// solution("TacoCat")
-// solution("AcZCbaBz")
-// solution("abcdefghijklmnopqrstuvwxyz")
+console.log(solution("TacoCat"))
+console.log(solution("AcZCbaBz"))
+console.log(solution("abcdefghijklmnopqrstuvwxyz"))
